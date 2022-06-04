8d84a0598759608134c71b1379efe444d3ec3ad4
Sqzrth <sqzrth2007@gmail.com>
2022-06-04 19:53:45 +0200

    shit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
-  if (document.documentElement.scrollTop > 350) {
-    document.getElementById("").className = "";
+  if (document.documentElement.scrollTop > 35) {
+    document.getElementById("introduction").className = "animation";
   }
 }
