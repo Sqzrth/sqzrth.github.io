be13bdf05848e6cbd92c00c4407a9ff2577865b7
Sqzrth <sqzrth2007@gmail.com>
2022-06-17 14:00:23 +0200

    a

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
-  if (document.documentElement.scrollTop > 10) {
-    document.getElementById("introduction").className = "animation";
+  if (document.documentElement.scrollTop > 200) {
+    document.getElementById("test").className = "animation";
   }
 }
