7f1a2ee87e6724fa0cee729199ee80a2a1ee7f0f
Sqzrth <sqzrth2007@gmail.com>
2022-09-24 18:39:20 +0200

    New

--- script.js
+++
@@ -1,7 +0,0 @@
-window.onscroll = function() {scroll()};
-
-function scroll() {
-  if (document.documentElement.scrollTop > 1) {
-    document.getElementById("animated").className = "animation";
-  }
-}
